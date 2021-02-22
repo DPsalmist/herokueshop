72b76bc74beb22d3d698dcebb0e01d1950387afb
Samuel Damilare <48826043+DPsalmist@users.noreply.github.com>
2021-02-22 02:54:42 +0100

    Update settings.py

--- myshop2/settings.py
+++ myshop2/settings.py
@@ -109,15 +109,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_ROOT = 'BASE_DIR / static'
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = 'BASE_DIR /static'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static')
+STATICFILES_DIR = 'BASE_DIR /static'
+#STATICFILES_DIR = (os.path.join(BASE_DIR, 'static')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CART_SESSION_ID = 'cart'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
